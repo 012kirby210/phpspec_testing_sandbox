194177c9e1647d814d15b9e4313a88c413c1614a
012kirby210 <012kirby210@gmail.com>
2023-07-25 20:48:45 +0200

    Création du premier matcher réutilisable.

--- spec/Matcher/BeGreaterMatcher.php
+++ spec/Matcher/BeGreaterMatcher.php
@@ -2,7 +2,38 @@
 
 namespace spec\Matcher;
 
-class BeGreaterMatcher
+use PhpSpec\Exception\Example\FailureException;
+use PhpSpec\Matcher\BasicMatcher;
+
+final class BeGreaterMatcher extends BasicMatcher
 {
 
+    protected function matches($subject, array $arguments): bool
+    {
+        return $subject > $arguments[0];
+    }
+
+    protected function getFailureException(string $name, $subject, array $arguments): FailureException
+    {
+        throw new FailureException(sprintf(
+            'La valeur devrait être 0, ce fut "%s"',
+            $subject
+        ));
+    }
+
+    protected function getNegativeFailureException(string $name, $subject, array $arguments): FailureException
+    {
+        throw new FailureException(sprintf(
+            'La valeur ne devrait pas être 0, ce fut "%s"',
+            $subject
+        ));
+    }
+
+    public function supports(string $name, $subject, array $arguments): bool
+    {
+        return in_array( $name, ['beGreater','beGreaterThan'])
+            && is_numeric($subject)
+            && count($arguments) === 1
+            && is_numeric($arguments[0]);
+    }
 }
