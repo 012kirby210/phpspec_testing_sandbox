b80dfed2f2d9b2e78c2e56f49f659bc02cd77556
012kirby210 <012kirby210@gmail.com>
2023-07-25 06:32:05 +0200

    Premiers matchers.

--- src/Entity/Dinosaur.php
+++ src/Entity/Dinosaur.php
@@ -4,4 +4,14 @@ namespace App\Entity;
 
 class Dinosaur
 {
+    private $length = 0;
+    public function getLength(): int
+    {
+        return $this->length;
+    }
+
+    public function setLength(int $length)
+    {
+        $this->length = $length;
+    }
 }
